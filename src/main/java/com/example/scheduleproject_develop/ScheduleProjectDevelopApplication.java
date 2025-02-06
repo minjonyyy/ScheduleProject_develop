1ca379c130dc705f38a32a987b72efafd42f0bfd
minjonyyy <minjonyyy@naver.com>
2025-02-06 23:19:10 +0900

    init commit

---
+++ src/main/java/com/example/scheduleproject_develop/ScheduleProjectDevelopApplication.java
@@ -0,0 +1,13 @@
+package com.example.scheduleproject_develop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleProjectDevelopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleProjectDevelopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/scheduleproject_develop/ScheduleProjectDevelopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scheduleproject_develop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleProjectDevelopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
