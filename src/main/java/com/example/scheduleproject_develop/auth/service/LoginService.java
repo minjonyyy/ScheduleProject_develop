13d3ce30af2b753ee363d8823067328091f1a231
minjonyyy <minjonyyy@naver.com>
2025-02-11 22:47:10 +0900

    feat : 비밀번호 암호화 구현

--- src/main/java/com/example/scheduleproject_develop/auth/service/LoginService.java
+++ src/main/java/com/example/scheduleproject_develop/auth/service/LoginService.java
@@ -1,6 +1,7 @@
 package com.example.scheduleproject_develop.auth.service;
 
 import com.example.scheduleproject_develop.auth.dto.LoginResponseDto;
+import com.example.scheduleproject_develop.config.PasswordEncoder;
 import com.example.scheduleproject_develop.user.dto.UserResponseDto;
 import com.example.scheduleproject_develop.user.entity.User;
 import com.example.scheduleproject_develop.user.repository.UserRepository;
@@ -14,13 +15,16 @@ import org.springframework.web.server.ResponseStatusException;
 public class LoginService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public UserResponseDto signup(String email, String username, String password) {
         userRepository.findByEmail(email).ifPresent(user -> {
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "이미 존재하는 이메일입니다.");
         });
 
-        User user = new User(email, username, password);
+        String encodedPassword = passwordEncoder.encode(password);
+
+        User user = new User(email, username, encodedPassword);
         User createdUser = userRepository.save(user);
 
         return new UserResponseDto(createdUser.getUserId(), createdUser.getUsername());
@@ -29,10 +33,13 @@ public class LoginService {
     public LoginResponseDto login(String email, String passsword){
         User findUser = userRepository.findUserByEmailOrElseThrow(email);
 
-        if(!findUser.getPassword().equals(passsword)){
+//        if(!findUser.getPassword().equals(passsword)){
+//            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "비밀번호가 일치하지 않습니다.");
+//        }
+
+        if(!passwordEncoder.matches(passsword, findUser.getPassword())){
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "비밀번호가 일치하지 않습니다.");
         }
-
         return new LoginResponseDto(findUser.getUserId());
 
     }
