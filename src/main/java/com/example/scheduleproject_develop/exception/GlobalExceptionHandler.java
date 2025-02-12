96c4667eb9aaf801f7b64e778ddee14bbbd59f5c
minjonyyy <minjonyyy@naver.com>
2025-02-13 00:01:34 +0900

    feat : 예외처리 추가하여 사용자에게 응답메세지 전달하는 기능 구현

--- src/main/java/com/example/scheduleproject_develop/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/scheduleproject_develop/exception/GlobalExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.example.scheduleproject_develop.exception;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -23,13 +26,17 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
     }
 
-//    @ExceptionHandler(Exception.class)
-//    public ResponseEntity<Map<String, String>> handleExceptions(Exception ex){
-//        Map<String, String> errors = new HashMap<>();
-//        ex.printStackTrace();
-//        errors.put("message", ex.getMessage());
-//        return ResponseEntity.status(ex).body(errors);
-//    }
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<Map<String, Object>> handleResponseStatusException(ResponseStatusException ex, HttpServletRequest request){
+        Map<String, Object> errors = new HashMap<>();
+
+        errors.put("status", ex.getStatusCode().value());
+        errors.put("message", ex.getMessage());
+        errors.put("path", request.getRequestURI());
+
+        return ResponseEntity.status(ex.getStatusCode()).body(errors);
+    }
+
 
 
 }
